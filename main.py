b5ceaf027381f3e751804a2228f95cac0bf4ff39
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2025-10-20 12:00:15 +0530

    Request method: POST

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 students = {
-    1: {"name": "Tarique Anjum", "age": 25}, 
-    2: {"name": "John", "age": 22}
+    1: {"name": "Tarique Anjum", "age": 25, "year": "Final Year"}, 
+    2: {"name": "John", "age": 22, "year": "Second Year"},
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student already exists"}
+    students[student_id] = student
+    return students[student_id]
+
 
 @app.get("/students")
 def get_students():
     return students
-
 
 
 @app.get("/student/{student_id}")
@@ -27,7 +41,6 @@ def get_student_by_name(*, student_id:int, student_name: Optional[str] = Query(N
     if student_id in students and students[student_id]["name"] == student_name:
         return students[student_id]
     return {"error": "Student not found"}
-
 
 
 @app.get("/about")
